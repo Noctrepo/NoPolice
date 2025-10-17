6dd569a60548a7b31511f6a537ac3b5b6d72998b
Nocturn <nocturn.xiv@gmail.com>
2025-10-16 21:53:26 -0400

    Visibility load checks

--- NoPolice/Plugin.cs
+++ NoPolice/Plugin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using Dalamud.Game.ClientState.Objects.SubKinds;
+using Dalamud.Game.ClientState.Objects.Types;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Ipc;
@@ -14,61 +15,71 @@ public sealed class Plugin : IDalamudPlugin
 {
     
     [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
     [PluginService] internal static IChatGui Chat { get; private set; } = null!;
     [PluginService] internal static IFramework Framework { get; private set; } = null!;
-    [PluginService] internal static IPluginLog PluginLog { get; private set; } = null!;
+    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
     [PluginService] internal static IObjectTable ObjectTable { get; private set; } = null!;
     
-    private ICallGateSubscriber<string, uint, string, object> AddToVoidList;
-    private ICallGateSubscriber<IEnumerable<string>> GetVoidListEntries;
-    private readonly CancellationTokenSource _cts = new();
-    private List<string> HiddenPlayers = new();
-    private DateTime lastScan;
-    
-    public Plugin(IDalamudPluginInterface pluginInterface)
+    private CancellationTokenSource  _cts = new CancellationTokenSource();
+
+    private List<string> Police = new()
+    {
+        "weeaboopolice",
+        "weebpolicelieutenant"
+    };
+
+    public Plugin()
+    {
+        if (!PluginInterface.GetIpcSubscriber<string, uint, string, object>("Visibility.AddToVoidList").HasFunction)
+        {
+            Chat.PrintError("Visibility plugin is required for this plugin to work.", "NoPolice");
+        }
+        
+        Framework.RunOnFrameworkThread(VisibilityPoll);
+        
+        ClientState.Login += OnLogin;
+    }
+
+    private void OnLogin()
+    {
+        if (!PluginInterface.GetIpcSubscriber<string, uint, string, object>("Visibility.AddToVoidList").HasFunction)
+        {
+            Chat.PrintError("Visibility plugin is required for this plugin to work.", "NoPolice");
+        }
+    }
+
+    private void VisibilityPoll()
     {
         try
         {
+            ICallGateSubscriber<string, uint, string, object>? AddToVoidList = null;
+            ICallGateSubscriber<IEnumerable<string>>? GetVoidListEntries = null;
+            List<string> VisibilityVoidList = new();
+
             AddToVoidList = PluginInterface.GetIpcSubscriber<string, uint, string, object>("Visibility.AddToVoidList");
-            
             GetVoidListEntries = PluginInterface.GetIpcSubscriber<IEnumerable<string>>("Visibility.GetVoidListEntries");
-            HiddenPlayers = GetVoidListEntries.InvokeFunc().ToList();
+            VisibilityVoidList = GetVoidListEntries.InvokeFunc().ToList();
 
-            PluginLog.Information("NoPolice Connected to PVis successfully.");
-        }
-        catch
-        {
-            PluginLog.Warning("PVis not loaded.");
-            Chat.Print("Visibility by SheepGoMeh required for this plugin to function.");
-        }
-
-        Framework.RunOnFrameworkThread(PollPlayers);
-    }
-    
-    private void PollPlayers()
-    {
-        foreach (var actor in ObjectTable)
-        {
-            if (actor is IPlayerCharacter player)
+            foreach (IGameObject actor in ObjectTable)
             {
+                if (actor is not IPlayerCharacter player) continue;
+
                 string name = player.Name.TextValue;
                 string pvisName = $"{player.Name.TextValue} {player.HomeWorld.RowId} NoPolice";
 
-                if (NormalizeName(name) == "weeaboopolice")
-                {
-                    if (!HiddenPlayers.Contains(pvisName))
-                    {
-                        AddToVoidList.InvokeAction(name, player.HomeWorld.RowId, "NoPolice");
-                    }
+                if (!Police.Contains(NormalizeName(name))) continue;
+                if (VisibilityVoidList.Contains(pvisName)) continue;
 
-                    HiddenPlayers = GetVoidListEntries.InvokeFunc().ToList();
-                }
-                
+                AddToVoidList!.InvokeAction(name, player.HomeWorld.RowId, "NoPolice");
+                VisibilityVoidList = GetVoidListEntries.InvokeFunc().ToList();
             }
+
+            Framework.RunOnTick(VisibilityPoll, TimeSpan.FromSeconds(3), cancellationToken: _cts.Token);
         }
-        
-        Framework.RunOnTick(PollPlayers, TimeSpan.FromSeconds(3), cancellationToken: _cts.Token);
+        catch (Exception e)
+        {
+            Framework.RunOnTick(VisibilityPoll, TimeSpan.FromSeconds(3), cancellationToken: _cts.Token);
+        }
     }
     
     private static string NormalizeName(string name)
