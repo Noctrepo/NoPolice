03da0bc9843f1f4dddff7df11bb71c1fe29955d8
0kie <82538043+0kie@users.noreply.github.com>
2026-02-03 02:30:19 -0300

    block their chat messages

---
+++ NoPolice/ChatHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using Dalamud.Game.Text;
+using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Game.Text.SeStringHandling.Payloads;
+
+namespace NoPolice;
+
+public class ChatHandler : IDisposable
+{
+    private readonly Configuration _config;
+
+    public ChatHandler(Configuration config)
+    {
+        _config = config;
+        Plugin.ChatGui.ChatMessage += OnChatMessage;
+    }
+
+    public void Dispose()
+    {
+        Plugin.ChatGui.ChatMessage -= OnChatMessage;
+    }
+
+    private void OnChatMessage(
+        XivChatType type,
+        int timestamp,
+        ref SeString sender,
+        ref SeString message,
+        ref bool isHandled)
+    {
+        if (isHandled)
+            return;
+
+        PlayerPayload? playerPayload = sender.Payloads
+            .SingleOrDefault(x => x is PlayerPayload) as PlayerPayload;
+
+        PlayerPayload? emotePlayerPayload = message.Payloads
+            .FirstOrDefault(x => x is PlayerPayload) as PlayerPayload;
+
+        bool isEmoteType = type is XivChatType.CustomEmote or XivChatType.StandardEmote;
+
+        if (playerPayload == null && (!isEmoteType || emotePlayerPayload == null))
+            return;
+
+        string? playerName = isEmoteType
+            ? emotePlayerPayload?.PlayerName
+            : playerPayload?.PlayerName;
+
+        if (string.IsNullOrEmpty(playerName))
+            return;
+
+        string normalizedName = new string(playerName.Where(char.IsLetter).ToArray())
+            .ToLowerInvariant();
+
+        if (_config.BlocklistNames.Contains(normalizedName))
+        {
+            isHandled = true;
+        }
+    }
+}
